a78b93555ba4df4eb94f6a41b8e9d9a8a343ce09
dayonad <onadujaao@gmail.com>
2024-10-09 09:11:05 +0100

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+import {
+    Box,
+    Container,
+    Stack,
+    Text,
+    useColorModeValue,
+} from '@chakra-ui/react';
+
+export default function Footer() {
+    // Get the current year dynamically
+    const currentYear = new Date().getFullYear();
+    return (
+        <Box
+     
+            mt={'4'}
+            bg={useColorModeValue('gray.50', 'gray.900')}
+            color={useColorModeValue('gray.700', 'gray.200')}
+            style={{
+                position: 'relative',
+                bottom: '0',
+                width: '100%',
+            }}>
+            <Container
+                as={Stack}
+                maxW={'6xl'}
+                py={4}
+                direction={{ base: 'column', md: 'row' }}
+                spacing={4}
+                justify={{ base: 'center', md: 'center' }}
+                align={{ base: 'center', md: 'center' }}>
+                <Text>© {currentYear} Currency Converter. All rights reserved</Text>
+            </Container>
+        </Box>
+    );
+}
